9f7bbc15afff523130edf34595606ca8e70b5854
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-02-27 18:57:38 +0000

    added Trig

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Let's build a calculator!
+from cmath import sqrt
 
 number_1 = input("Number 1 :\n")
-operation = input("Operation(+, -, *, /) :\n")
+operation = input("Operation(+, -, *, /), Trig :\n").lower()
 number_2 = input("Number 2 :\n")
 
 #different operations:
@@ -13,5 +14,7 @@ if operation == "*":
   print(int(number_1) * int(number_2))
 if operation == "/":
   print(int(number_1) / int(number_2))  
+if operation == "trig":
+  print(sqrt(number_1**2 + number_2**2))
 else:
   print("Wrong input!")
